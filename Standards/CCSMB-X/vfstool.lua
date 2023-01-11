d67d2dd4d86ab3c118887a04389a05e6a9d4625d
walksanatora <walkerffo22@gmail.com>
2023-01-11 13:48:03 -0800

    comment example

--- Standards/CCSMB-X/vfstool.lua
+++ Standards/CCSMB-X/vfstool.lua
@@ -29,6 +29,8 @@ local compression = table.remove(opts,1)
 local output = table.remove(opts,1)
 local files = opts
 
+--this function is basically a VFS encoder function
+--it takes a path and then recursively adds it to a table
 local function gen_disk(path)
     sleep()
     local path = path or ""
@@ -45,31 +47,36 @@ local function gen_disk(path)
     end
     return tree
 end
-
+--boilerplate to make sure direction is valid
 if not ((direction == "build") or (direction == "extract")) then
     error("arg #1 must be either build or extract not "..direction)
 end
 
 if direction == "build" then
+    --make sure all input files exist
     for _,v in ipairs(files) do
         local v = shell.resolve(v)
         if not fs.exists(v) then error("File/Folder does not exist: "..v) end
     end
-    if (compression == "compress") and (not loaded_ld) then error("Unnable to load LibDeflate"..ld) end
+
+    --resolve path to output file
     local sro = shell.resolve(output)
+    --check if we are okay to overwrite it
     if fs.exists(sro) and (not settings.get("vfstool.overwrite")) then
         print(output.." allready exist\nOverwrite Y/N:")
         c = read()
         if not string.find(c,"Y") then print("Aborted") return end
     end
+    --create the table to store our VFS
     local final = {}
     print("starting tree gen")
+    --add all targets to the root of the image
     for _,v in ipairs(files) do
         local sr = shell.resolve(v)
         print("adding: "..v)
         if fs.isDir(sr) then
             final[fs.getName(v)] = gen_disk(sr)
-        else
+        else --why have diskgen do it when we can do it here
             local hand = fs.open(sr,'rb')
             local con = hand.readAll()
             final[fs.getName(v)] = con
@@ -80,18 +87,19 @@ if direction == "build" then
     local ser = textutils.serialise(final,{compact=true})
     print("writing")
 
-    local handle = fs.open(sro,'wb')
+    local handle = fs.open(sro,'wb') --write the serialised data to the file
     handle.write(ser)
     handle.close()
 else
-    if not fs.exists(shell.resolve(output)) then
+    if not fs.exists(shell.resolve(output)) then --make sure input image exist
         error("img file does not exist")
     end
     local rpath = shell.resolve(files[1])
-    if fs.isReadOnly(rpath) then
+    if fs.isReadOnly(rpath) then --make sure we can write to output
         error(files[1].." is not writable")
     end
 
+    --really big boilerplate to make sure what we want to do to output
     local dir_action = settings.get("vfstool.dir_action")
     if fs.exists(rpath) and (dir_action ~= "M") and (dir_action ~= "O") and (dir_action ~= "C") and fs.exists(rpath) then
         print("directory "..files[1].."allready exist, Merge,Overwrite,Cancel?")
@@ -119,6 +127,8 @@ else
         end
     end
 
+    --the sister to gen_disk, takes a the current path we are working in and the files to unpack
+    --then writes them
     function unpack_vfs(path,files)
         print("making: "..path)
         fs.makeDir(fs.combine(rpath,path))
@@ -135,14 +145,14 @@ else
         end
     end
 
-    local diskf = fs.open(shell.resolve(output),"rb")
+    local diskf = fs.open(shell.resolve(output),"rb") --read content
     local content = diskf.readAll()
     diskf.close()
-    local tcontents = textutils.unserialize(content)
+    local tcontents = textutils.unserialize(content) --decode to table
     local sdir = shell.dir()
     print("unpacking vfs")
-    unpack_vfs("",tcontents)
+    unpack_vfs("",tcontents) --unpack VFS
     print("done")
-    shell.setDir(sdir)
+    shell.setDir(sdir) --head back to where we were
 
 end
